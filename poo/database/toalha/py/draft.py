596c11dedd30609d8d8c6c37742be8730da815c2
Ana Cambraia <anabeatrizsilvacambraia@gmail.com>
2025-09-16 20:24:58 +0000

    aprendendo poo

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,10 @@
+class Towel: 
+    def __init__(self): #construir meu obejto
+        self.color = 'red' #atributos
+        self.size = 'p'
+        self.wetness = '0'
+
+towel = Towel() #objetosq
+print(towel.color)
+print(towel.size)
+print(towel.wetness)
