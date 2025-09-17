ed7e8092d0a491fd2b378cbb8f5b76833fff4637
Ana Cambraia <anabeatrizsilvacambraia@gmail.com>
2025-09-17 23:57:28 +0000

    Assisti um video aula e fiz mais um pouco de codigo

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,10 +1,14 @@
 class Towel: 
-    def __init__(self): #construir meu obejto
-        self.color = 'red' #atributos
-        self.size = 'p'
-        self.wetness = '0'
+    def __init__(self, color : str, size : str): #construir meu obejto e definir atributos
+        self.color : str = 'red' #atributos
+        self.size : str = 'p'
+        self.wetness : int = '0' 
 
-towel = Towel() #objetosq
-print(towel.color)
-print(towel.size)
-print(towel.wetness)
+    def getMaxWetness(self) -> int:
+        if self.size == "P":
+            return 10
+        if self.size == "M":
+            return 20
+        if self.size == "G":
+            return 30
+        return 0
