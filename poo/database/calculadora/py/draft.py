d300454790860b6d2b5ca445726a53ee8e25527a
Ana Cambraia <anabeatrizsilvacambraia@gmail.com>
2025-10-02 22:29:35 +0000

    finalizando a bateria

--- poo/database/calculadora/py/draft.py
+++ poo/database/calculadora/py/draft.py
@@ -3,11 +3,31 @@ class Calculator:
         self.battery : int = 0
         self.batteryMax : int = batteryMax
         self.display : float = 0.0
+
+    def charge(self, value):
+        self.battery += value
+        if self.battery > self.batteryMax:
+            self.battery = self.batteryMax
+
+    def sum(self, a : int, b : int):
+        if self.battery == 0:
+            print("fail: bateria insuficiente")
+            return
+        self.battery -= 1
+        self.display = a + b
+
+    def division(self, num : int, den : int):
+        if self.battery == 0:
+            print("fail: bateria insuficiente")
+            return
+        self.battery -= 1
+        if den == 0:
+            print("fail: divisao por zero")
+            return
+        self.display = num / den
+        
+
     
-    def charge(self, value : int) -> None:
-        if 
-
-
     def __str__(self) -> str:
         return f"display = {self.display:.2f}, battery = {self.battery}"
 def main():
@@ -22,4 +42,12 @@ def main():
             calculadora = Calculator(int(args[1]))
         elif args[0] == "show":
             print(calculadora)
+        elif args[0] == "charge":
+            calculadora.charge(int(args[1]))
+        elif args[0] == "sum":
+            calculadora.sum(int(args[1]), int(args[2]))
+        elif args[0] == "div":
+            calculadora.division(int(args[1]), int(args[2]))
+
+    
 main()
